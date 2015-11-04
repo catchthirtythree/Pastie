d8af5a0c68f6f8d5a20228d7f0fd6ef1d9b54ea7
Michael <catchthirtythree@users.noreply.github.com>
2015-11-04 18:11:38 -0500

    Update Imgur.cs

--- Pastie/Services/Imgur.cs
+++ Pastie/Services/Imgur.cs
@@ -17,7 +17,7 @@ namespace Utilities
 {
     class Imgur
     {
-        private static string CLIENT_ID = "be97e107a50ee57";
+        private static string CLIENT_ID = "";
         private static string UPLOAD_URL = "https://api.imgur.com/3/upload.json";
 
         public ImgurResponse UploadImage(Image bmp)
